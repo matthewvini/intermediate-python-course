5c945029d8648f2ab9c368b17db4025784e97058
[Matthew vini] <[clintu000&@gmail.com]>
2020-02-26 12:50:11 +0530

    add user inputs for rolling dice

--- dice_roller.py
+++ dice_roller.py
@@ -2,14 +2,21 @@
 
 def main():
     import random
-    dice_rolls = 2
+    dice_rolls = int(input("How many dice would you like to roll ?"))
+    dice_size = int(input('How many sides are the dice?'))
     dice_sum = 0
     for i in range(0,dice_rolls):
         """ for every index in a range starting after 0 until the value is equal to dice_rolls, do this :"""
-        roll = random.randint(1,6)
+        roll = random.randint(1,dice_size)
         dice_sum+=roll
-        print(f'You rolled a {roll}')
-    print(f'You have rolled a total of {dice_sum}')    
+        if roll == 1:
+            print(f'You rolled {roll}! Critical Fail')
+        elif roll == dice_size:
+            print(f'You rolled {roll}! Critical Success')
+        else:
+
+            print(f'You rolled a {roll}')#f-string functionality.
+    print(f'You have rolled a total of {dice_sum}')
 
 
 if __name__== "__main__":
